2f419506ccda3fa48de60fb88b0534082098fe16
Alex Lee <ahjlee712@gmail.com>
2021-05-18 18:15:03 -0400

    Set initial API call for product infol

--- client/components/Overview/Overview.jsx
+++ client/components/Overview/Overview.jsx
@@ -1,9 +1,19 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import api from '../../../api.js';
 
 const Overview = ({product_id}) => {
+  const [currentProduct, setCurrentProduct] = useState({});
+
+  //get current product info
+  useEffect(() => {
+    api.getProduct(product_id)
+      .then(res => setCurrentProduct(res.data))
+      .catch(err => console.error('Cannot retrieve Product Info', err))
+  }, [])
+
   return(
     <section className = 'overview-section'>
-     {product_id}
+
     </section>
   )
 };
